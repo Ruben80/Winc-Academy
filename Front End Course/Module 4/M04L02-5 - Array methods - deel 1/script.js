e60956e2ec2e308e07c404211e9043f81857a472
Ruben Bos <bos.ruben@gmail.com>
2020-07-07 18:35:58 +0200

    Added Array methods - deel 1

---
+++ Front End Course/Module 4/M04L02-5 - Array methods - deel 1/script.js
@@ -0,0 +1,44 @@
+// A Schrijf een JavaScript functie om het woord "cool" aan een array met strings toe te voegen.  Gebruik de .push method.
+
+// const addTheWordCool = ["nice", "awesome", "tof"];
+// addTheWordCool.push("cool");
+// console.log("Add cool:", addTheWordCool);
+
+// B Schrijf een JavaScript functie die het aantal elementen in een array retourneert.
+
+// const amountOfElementsInArray = ['appels', 'peren', 'citroenen']; 
+// console.log(amountOfElementsInArray.length); 
+  
+// C Schrijf een JavaScript functie om het éérste element uit een array te retourneren. 
+
+// let array = ["Belgie", "Nederland", "Luxemburg"];
+// let selectBelgiumFromBenelux = array.shift();
+// console.log(selectBelgiumFromBenelux);
+
+// D Schrijf een JavaScript functie om het laatste element uit een array te retourneren.
+
+// let array = ["Haas", "Cavia", "Kip", "Schildpad"];
+// let lastElementInArray = array.pop();
+// console.log(lastElementInArray);
+
+// E Schrijf een JavaScript functie om het eerste element uit een array te halen en het restant te retourneren . Gebruik .slice én de alternatieve optie .splice. Wat is het verschil tussen deze functies? Hint: mutability.
+
+// const presidents = ["Trump", "Obama", "Bush", "Clinton"] 
+  
+// console.log(presidents.slice(1)); // ["Obama", "Bush", "Clinton"]
+// console.log(presidents.splice(4,1)); // ["Obama", "Bush", "Clinton"]
+
+// F Write a simple JavaScript program to join all elements of the following array into a string (with spaces). 
+
+// const stringsTogether = ['Winc', 'Academy', 'is', 'leuk', ';-}'];
+// console.log(stringsTogether.join (" ")); 
+
+// G Schrijf een functie die 2 arrays ['array 1'] ['array2'] combineert tot 1 array ['array1', 'array2']
+
+// const combineArrays = function(array1, array2) {
+//   return array1.concat(array2);
+// };
+
+// console.log(combineArrays([1, 2, 3], [4, 5, 6]));
+
+// resultaat: [1,2,3,4,5,6]
