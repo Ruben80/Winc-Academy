f4235b6255e6aa53880a756c82951014111ef2da
Ruben Bos <bos.ruben@gmail.com>
2020-07-16 14:51:27 +0200

    Second Commit

--- Front End Course/Module 5/M05L03-3 - Async-await - BOILERPLATE/script.js
+++
@@ -1,8 +0,0 @@
-// Verplaats het aanroepen van de functie getData() naar script.js. Je wilt de functie getData() afwachten. Hoe krijg je toegang tot het keyword await? Door deze in een async functie te zetten. Dus maak een asyncronous functie waarin je getData() afwacht.
-
-const doSomethingWithData = async function () {
-    const data = await getData();
-    console.log("Here is your data:", data);
-};
-
-doSomethingWithData();
